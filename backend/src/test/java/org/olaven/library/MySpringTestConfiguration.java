af6e4330ec2a6c81bb89655903e6aaf8d70b14a0
olaven <olavsundfoer@gmail.com>
2019-02-19 21:25:04 +0100

    added custom annotation

---
+++ backend/src/test/java/org/olaven/library/MySpringTestConfiguration.java
@@ -0,0 +1,25 @@
+package org.olaven.library;
+
+
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.Documented;
+
+
+@ExtendWith(SpringExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) // needed for @BeforeAll
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD) // cleaning db
+@Target(
+        ElementType.TYPE // can use on classes and interfaces
+)
+@Retention(RetentionPolicy.RUNTIME) //specify it should end up in the bytecode and be readable using reflection
+@Documented
+public @interface MySpringTestConfiguration {}
